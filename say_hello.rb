2420997412ec1babb126d5e59de12cea45dbcc66
Alfredo Diaz <mail4alfredo@gmail.com>
2019-08-14 03:19:31 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,7 +1,7 @@
-def say_hello(name = "Kent Beck")
+def say_hello(name = "Ruby programmer")
   puts "Hello #{name}!"
 end
 
-def say_hello(no name = "Ruby programmer")
-  puts "Hello #{no name}!"
+def say_hello
+  puts "Hello #{Kent Beck}!"
 end
